f8ad51b62e8ceb59166068c06ea9330b8eeea21a
Kazi Bablu <33626354+Kazi-Bablu@users.noreply.github.com>
2019-09-11 17:40:52 +0600

    user login complete

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,48 @@
 <template>
-  <div class="container col-md-6 mt-5">
-    <h2>Login</h2>
-    <br>
-    <form>
-      <div class="form-group">
-        <label>Email address</label>
-        <input type="email" class="form-control" placeholder="Enter email">
-        <small class="form-text text-danger">Show errors here</small>
-      </div>
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control" placeholder="Password">
-        <small class="form-text text-danger">Show errors here</small>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-    <br>
-    <p>Don't have a account ? <nuxt-link to="/register">Register</nuxt-link></p>
-  </div>
+    <div class="container col-md-6 mt-5">
+        <h2>Login</h2>
+        <br>
+        <form @submit.prevent="submit">
+            <div class="form-group">
+                <label>Email address</label>
+                <input v-model.trim="form.email" type="email" class="form-control" placeholder="Enter email" autofocus>
+                <small class="form-text text-danger">Show errors here</small>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input v-model.trim="form.password" type="password" class="form-control" placeholder="Password">
+                <small class="form-text text-danger">Show errors here</small>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <br>
+        <p>Don't have a account ?
+            <nuxt-link to="/register">Register</nuxt-link>
+        </p>
+    </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      async submit() {
+        await this.$auth.loginWith('local', {
+          data: {
+            email: this.form.email,
+            password: this.form.password
+          }
+        })
+
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
